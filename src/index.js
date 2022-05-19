5ffaafeb5d1f35247972028603c3224924eca516
Juani <jrm3301@gmail.com>
2022-05-19 18:02:52 +0100

    cambio de rama

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 
+import { getDatabase, ref, child, push, update } from "firebase/database";
+
 const firebaseConfig = {
   apiKey: process.env.API_KEY || "API_KEY_VACIA",
   authDomain: process.env.AUTH_DOMAIN || "AUTH_DOMAIN_VACIO",
@@ -7,3 +9,27 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+function writeNewPost(uid, username, picture, title, body) {
+  const db = getDatabase();
+
+  // A post entry.
+  const postData = {
+    author: username,
+    uid,
+    body,
+    title,
+    starCount: 0,
+    authorPic: picture
+  };
+
+  // Get a key for a new Post.
+  const newPostKey = push(child(ref(db), "posts")).key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  const updates = {};
+  updates["/posts/" + newPostKey] = postData;
+  updates["/user-posts/" + uid + "/" + newPostKey] = postData;
+
+  return update(ref(db), updates);
+}
